77f640f7e9aaec415f941cca862eb96b8caf1e82
Maxym Tovstenko <maxym.t28@gmail.com>
2016-10-17 13:39:52 +0300

    Update Main.java
    Was created constants "SPACE_CHAR", "FIRST_NUMBER_INDEX", "SECOND_NUMBER_INDEX".
    Changed types of lists from ArrayList to List.
    Changed field name "m" to "main".

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,27 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Comparator;
 import java.util.Scanner;
 
 public class Main {
+    private static final String SPACE_CHAR = "\\s+";
+    private static final int FIRST_NUMBER_INDEX = 0;
+    private static final int SECOND_NUMBER_INDEX = 1;
 
     public static void main(String[] args) {
-        Main m = new Main();
-        String inputString = m.readInput();
-        ArrayList<Integer> numbers = m.splitNumbers(inputString);
-        m.findDistance(numbers);
+        Main main = new Main();
+        String inputString = main.readInput();
+        List<Integer> numbers = main.splitNumbers(inputString);
+        main.findDistance(numbers);
     }
 
-    private void findDistance(ArrayList<Integer> numbers) {
-        ArrayList<Integer> sortedNumbers = new ArrayList<>(numbers);
+    private void findDistance(List<Integer> numbers) {
+        List<Integer> sortedNumbers = new ArrayList<>(numbers);
         sortedNumbers.sort(Comparator.naturalOrder());
-        int pos1 = (numbers.indexOf(sortedNumbers.get(0)));
-        int pos2 = (numbers.indexOf(sortedNumbers.get(1)));
+        int pos1 = (numbers.indexOf(sortedNumbers.get(FIRST_NUMBER_INDEX)));
+        int pos2 = (numbers.indexOf(sortedNumbers.get(SECOND_NUMBER_INDEX)));
         int result = (Math.abs(pos1 - pos2));
         System.out.println(result);
     }
@@ -27,9 +31,9 @@ public class Main {
         return scanner.nextLine();
     }
 
-    private ArrayList<Integer> splitNumbers(String inputString) {
-        String[] numbsArray = inputString.split("\\s+");
-        ArrayList<Integer> numbers = new ArrayList<>();
+    private List<Integer> splitNumbers(String inputString) {
+        String[] numbsArray = inputString.split(SPACE_CHAR);
+        List<Integer> numbers = new ArrayList<>();
         for (String number : numbsArray) {
             numbers.add(Integer.parseInt(number));
         }
